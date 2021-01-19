2bf4f628b635d3ad31330bb26d2b5f9b9ef64a00
Nathan Culver <s6nculve@gmail.com>
2021-01-19 12:00:25 -0800

    Added logic.

--- light-switch/src/App.js
+++ light-switch/src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import './App.css';
 
+import White from './Components/White.js'
+import Yellow from './Components/Yellow.js'
+
 const white = "https://image.flaticon.com/icons/png/512/32/32177.png";
 const yellow =
   "https://i.pinimg.com/originals/92/94/ba/9294badee7b8f3d93fa9bc6c874641b2.png";
@@ -10,6 +13,16 @@ export default function App() {
   const [lightOn, setLightOn] = useState(true);
 
   return (
-    <div onClick={() => setLightOn(!lightOn)} className="App"></div>
+    <div onClick={() => setLightOn(!lightOn)} className="App">
+      {lightOn === false ? (
+        <White 
+          white={white}
+        />
+      ) : (
+        <Yellow 
+          yellow={yellow}
+        />
+      )}
+    </div>
   );
 }
